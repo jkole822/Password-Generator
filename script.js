b18d43494fce3614d1a1001a28d8de9e49a20c44
Kole Gasior <jkole822@gmail.com>
2020-10-21 18:31:35 -0400

    create generatePassword function and add to function strings for characters, prompts for user, and object to store user choice

--- script.js
+++ script.js
@@ -3,11 +3,49 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+	const getRandomInt = max => {
+		return Math.floor(Math.random * max);
+	};
+	const generatePassword = () => {
+		const alphabet = "abcdefghijklmnopqrstuvwxyz";
+		const specialChars = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+		// Ask the user for input and assign selections to respective variables.
+		// numChar will determine the endpoint for the while loop below
+		const numChar = parseInt(
+			prompt(
+				"Input an integer value between 8-128 for the number of characters included in your password."
+			)
+		);
+		const selectLow = confirm("Include lowercase characters?");
+		const selectUp = confirm("Include uppercase characters?");
+		const selectNum = confirm("Include numeric characters?");
+		const selectSpec = confirm("Include special characters?");
+
+		// Create charTypes object that assigns a value to objects containing keys that specify the type of
+		// character and whether the user selected that character to be used to generate a password
+		const charTypes = {
+			0: {
+				category: "lowercase",
+				active: selectLow,
+			},
+			1: {
+				category: "uppercase",
+				active: selectUp,
+			},
+			2: {
+				category: "numeric",
+				active: selectNum,
+			},
+			3: {
+				category: "special",
+				active: selectSpec,
+			},
+		};
+	};
+
 	var password = generatePassword();
 	var passwordText = document.querySelector("#password");
-
-	const generatePassword = () => {};
-
 	passwordText.value = password;
 }
 
